db09fc8a90f360728d92e9c8b8fe1932b871821c
khodll <28khodee@gmail.com>
2019-10-31 16:49:50 +1100

    new classes ingredient & importingredient

---
+++ app/src/main/java/com/example/infs3634app/model/IngredientImport.java
@@ -0,0 +1,15 @@
+package com.example.infs3634app.model;
+
+import java.util.ArrayList;
+
+public class IngredientImport {
+    private ArrayList<Ingredient> ingredients = null;
+
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
